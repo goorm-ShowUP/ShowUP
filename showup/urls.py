3319118ae3607dfcac29e19edad762d795fcd372
Yu Lim Kim <yulime0605@naver.com>
2023-07-13 11:11:24 +0900

    templates, static 폴더 추가

--- showup/urls.py
+++ showup/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     # path('show_up/', include('show_up.urls')),
     path("admin/", admin.site.urls),
+    path('', include('show_up.urls')),
 ]
